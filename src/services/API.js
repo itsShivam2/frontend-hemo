98ef6da7eea7527974a6858621ebb866f022d021
itsShivam2 <135848325+itsShivam2@users.noreply.github.com>
2024-06-23 21:14:50 +0530

    Initial commit

---
+++ src/services/API.js
@@ -0,0 +1,13 @@
+import axios from "axios"
+
+const API=axios.create({baseURL:"https://bloodbank-zi0j.onrender.com/api/v1"})
+
+API.interceptors.request.use((req)=>{
+if(localStorage.getItem("token")){
+    req.headers.Authorization=`Bearer ${localStorage.getItem("token")}`;
+
+}
+return req;
+})
+
+export default API;
